b5973541d2842a049e308ca0e87c8455adfda105
Shyam Sagar Singhal <singhals@avaya.com>
2020-05-23 22:24:40 +0530

    Hello World

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C Charp OOPS Concept");
+            Console.WriteLine("Hello World");
         }
     }
 }
